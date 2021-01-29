51f47946ac3e11d8d47719ce1e076b1db093c4b6
cocodori <moradulee@gmail.com>
2021-01-29 14:32:39 +0900

    gradle npm install추가

--- src/main/java/com/dunk/django/member/MemberAdapter.java
+++ src/main/java/com/dunk/django/member/MemberAdapter.java
@@ -6,14 +6,14 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 
 import java.io.Serializable;
-import java.util.List;
+import java.util.Arrays;
 
 @Getter
 public class MemberAdapter extends User implements Serializable {
     private Member member;
 
     public MemberAdapter(Member member) {
-        super(member.getEmail(), member.getPassword(), List.of(new SimpleGrantedAuthority(member.getRoleKey())));
+        super(member.getEmail(), member.getPassword(), Arrays.asList(new SimpleGrantedAuthority(member.getRoleKey())));
         this.member = member;
     }
 }
