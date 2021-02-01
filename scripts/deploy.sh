f9244cd149598860f0d7377d3d7d85215922764b
cocodori <moradulee@gmail.com>
2021-02-01 13:56:10 +0900

    Travis CI - S3 - CodeDeploy - 배포

---
+++ scripts/deploy.sh
@@ -0,0 +1,36 @@
+!#/bin/bash
+
+REPOSITORY=/home/ec2-user/app/step2
+PROJECT_NAME=recipe.com
+
+echo ">Build 파일 복사"
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
+
+echo "> 현재 구동 중인 애플리케이션 PID 확인"
+
+CURRENT_PID=$(pgrep -fl recipe.com | grep jar | awk '{print $1}')
+
+echo "> 현재 구동 중인 애플리케이션 PID : $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+  echo ">현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+echo "> 새 애플리케이션 배포"
+
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+echo "> JAR Name : $JAR_NAME"
+
+echo "> $JAR_NAME 실행 권한 추가"
+
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
+nohup java -jar -Dspring.profiles.active=runtime \
+  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
