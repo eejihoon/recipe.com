f5fb244cf29c7c9bbba879b19c3273ef99f62f02
cocodori <moradulee@gmail.com>
2021-02-03 17:15:25 +0900

    EmailService - @Profile({"runtime", "runtime2"}) 설정

--- src/main/java/com/dunk/django/mail/HtmlEmailServiceImpl.java
+++ src/main/java/com/dunk/django/mail/HtmlEmailServiceImpl.java
@@ -11,7 +11,7 @@ import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
 @Slf4j
-@Profile("runtime")
+@Profile({"runtime", "runtime2"})
 @RequiredArgsConstructor
 @Component
 public class HtmlEmailServiceImpl implements EmailService {
