4188ce9fbe9c2a24237ca9a7a5a311299e444934
cocodori <moradulee@gmail.com>
2021-02-03 15:58:32 +0900

    script파일 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
@@ -12,12 +12,12 @@ echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-echo "> 새 애플리케이션 배포"
+echo "> 새 어플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
-echo "> JAR NAME : $JAR_NAME"
+echo "> JAR Name: $JAR_NAME"
 
-echo "> $JAR_NAME 에 실행 권한 부여"
+echo "> $JAR_NAME 에 실행권한 추가"
 
 chmod +x $JAR_NAME
 
@@ -27,6 +27,7 @@ IDLE_PROFILE=$(find_idle_profile)
 
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 
-nohup java -jar -Dspring.profiles.active=$IDLE_PROFILE \
-        -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-mail.properties \
+nohup java -jar \
+        -Dspring.profiles.active=$IDLE_PROFILE \
+        -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-mail.properties,/home/ec2-user/app/application-$IDLE_PROFILE.properties \
         $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
