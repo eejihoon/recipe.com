d24231a16b5d2089f25ba7603d73281890378d91
cocodori <moradulee@gmail.com>
2021-02-03 17:39:38 +0900

    health.sh 수정

--- scripts/health.sh
+++ scripts/health.sh
@@ -15,7 +15,7 @@ sleep 10
 for RETRY_COUNT in {1..10}
 do
   RESPONSE=$(curl -s http://localhost:${IDLE_PORT}/profile)
-  UP_COUNT=$(echo ${RESPONSE} | grep 'real' | wc -l)
+  UP_COUNT=$(echo ${RESPONSE} | grep 'runtime' | wc -l)
 
   if [ ${UP_COUNT} -ge 1 ]
   then # $up_count >= 1 ("real" 문자열이 있는지 검증)
