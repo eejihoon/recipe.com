c86fb113cd650799537b621a91bee3b3dbbf35b4
cocodori <moradulee@gmail.com>
2021-03-02 22:14:50 +0900

    Update : 스케쥴러 메서드명 변경 및 로그 출력 방식 수정

--- src/main/java/com/recipe/schedule/SchedulerTask.java
+++ src/main/java/com/recipe/schedule/SchedulerTask.java
@@ -20,14 +20,18 @@ public class SchedulerTask {
     *   매일 오전 04시에 한 번만 실행
     * */
     @Scheduled(cron = "0 0 4 * * ?")
-    public void schdulerTest() {
+    public void athenticationTimeOutMemberDeleteTask() {
         log.info("Schedule Task Run... Time : {} ", LocalDateTime.now());
         List<Member> allMember = memberRepository.findAll();
 
+        log.info("==========================================================|");
         for (Member member : allMember) {
             if (member.isAuthenticationTimeOut()) {
+                log.info("---------Target Member email: {} ", member.getEmail());
+                log.info("---------Target Member nickname: {} ", member.getNickname());
                 memberRepository.delete(member);
             }
         }
+        log.info("==========================================================|");
     }
 }
